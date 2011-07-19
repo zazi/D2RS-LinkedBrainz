13abc35c46f6ee3f256e1b700da101eaaa3214b0
Bob Ferris <zazi@smiy.org>
2011-07-19 18:03:12 +0200

    - [ADDED] Utils.java to have a helper class for the Translators 	- current method(s): validates a give URI against a base URI (for consistent base URI check of information services)

--- D2RS-LinkedBrainz/src/main/java/translators/MySpaceTranslator.java
+++ D2RS-LinkedBrainz/src/main/java/translators/MySpaceTranslator.java
@@ -1,22 +1,45 @@
 package translators;
 
+import translators.util.Utils;
 import de.fuberlin.wiwiss.d2rq.values.Translator;
-import java.lang.String;
 
-public class MySpaceTranslator implements Translator {
+/**
+ * This translator translates MySpace specific page URLs into DBTune specific
+ * resource URIs.
+ * 
+ * TODO: implementing a proper URL clean-up task (everything should have a
+ * http://myspace.com/-based URL at the end before transferring this URL)
+ * 
+ * @author kurtjx
+ * @author zazi
+ * 
+ */
+public class MySpaceTranslator implements Translator
+{
 
-	public MySpaceTranslator(){
-		
+	public MySpaceTranslator()
+	{
+
 	}
 
-	public String toDBValue(String rdfValue) {
+	public String toDBValue(String rdfValue)
+	{
 		return rdfValue.replaceFirst("http://dbtune.org/myspace/",
-					     "http://www.myspace.com/");
-	}	
+				"http://myspace.com/");
+	}
 
-	public String toRDFValue(String dbValue) {
-		return dbValue.replaceFirst("http://www.myspace.com/",
-					    "http://dbtune.org/myspace/");
+	public String toRDFValue(String dbValue)
+	{
+		if (Utils.cleanUpBaseURI(dbValue, "http://myspace.com/") != null)
+		{
+			return Utils.cleanUpBaseURI(dbValue, "http://myspace.com/")
+					.replaceFirst("http://myspace.com/",
+							"http://dbtune.org/myspace/");
+		} 
+		// URI does not belong to this information service
+		else
+		{
+			return null;
+		}
 	}
 }
-

113f8669f7f146d31d53681924363c9c87d6e78b
Bob Ferris <zazi@smiy.org>
2011-07-20 13:17:06 +0200

    WikipediaTranslator.java: 	- [FIXED] base URI handling, i.e. the base URI is now language independent and every other base URI will converted to such one if possible Utils.java: 	- initial import (!) d2rs-0.7-LinkedBrainz.jar: 	- [UPDATED] !

--- D2RS-LinkedBrainz/src/main/java/translators/WikipediaTranslator.java
+++ D2RS-LinkedBrainz/src/main/java/translators/WikipediaTranslator.java
@@ -3,34 +3,39 @@ package translators;
 import de.fuberlin.wiwiss.d2rq.values.Translator;
 import java.lang.String;
 
+import translators.util.Utils;
+
 /**
-TODO FIXME: different languages in wikipedia break this
-(e.g. http://fr.wikipedia.org/wiki/The_Arcade_Fire)
-
-SELECT DISTINCT * WHERE {
-<http://musicbrainz.org/artist/52074ba6-e495-4ef3-9bb4-0703888a9f68#_>
-owl:sameAs ?o .  } LIMIT 10 
-
-or a translation table with a custom Java class that returns null for
-unwanted database values.
+ * @author kurtjx
+ * @author zazi
+ * 
  */
-public class WikipediaTranslator implements Translator {
+public class WikipediaTranslator implements Translator
+{
 
-	public WikipediaTranslator(){
-		
+	public WikipediaTranslator()
+	{
+
 	}
 
-	public String toDBValue(String rdfValue) {
+	public String toDBValue(String rdfValue)
+	{
 		return rdfValue.replaceFirst("http://dbpedia.org/resource/",
-					     "http://en.wikipedia.org/wiki/");
-	}	
+				"http://wikipedia.org/wiki/");
+	}
 
-	public String toRDFValue(String dbValue) {
-	    if(!dbValue.contains("en.wikipedia")){
-		return null; // FIXME horrible hack only deals w/ English pages
-		    }
-	    return dbValue.replaceFirst("http://en.wikipedia.org/wiki/",
-					"http://dbpedia.org/resource/");
+	public String toRDFValue(String dbValue)
+	{
+		if (Utils.cleanUpBaseURI(dbValue, "http://wikipedia.org/wiki/") != null)
+		{
+			return Utils.cleanUpBaseURI(dbValue, "http://wikipedia.org/wiki/")
+					.replaceFirst("http://wikipedia.org/wiki/",
+							"http://dbpedia.org/resource/");
+		}
+		// URI does not belong to this information service
+		else
+		{
+			return null;
+		}
 	}
 }
-
